426943e87e2634881300719b5815ab61ad7a1649
xinxinn <leexin@pixel-pine.com>
2020-12-01 20:33:28 +0800

    set up react navigation dependencies

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <NavigationContainer>
+      {
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>Universal React with Expo</Text>
+        </View>
+      }
+    </NavigationContainer>
   );
 }
